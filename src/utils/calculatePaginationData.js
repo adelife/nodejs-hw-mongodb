40e6d632f642fd25f22f590908c8c5d088fddeef
adelife <142346901+adelife@users.noreply.github.com>
2024-09-13 14:48:18 +0300

    Update calculatePaginationData.js

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,9 +1,13 @@
-export const calculatePaginationData = (totalItems , perPage, page) => {
-    const totalPages = (totalItems / perPage);
-    const hasNextPage = Boolean(totalPages - page);
+export const calculatePaginationData = ({total , perPage, page}) => {
+    const totalPages =  Math.ceil(total / perPage);
+    // const hasNextPage = Boolean((totalPages - page) > 0);
+    const hasNextPage = page < totalPages;
     const hasPreviousPage = page !== 1;
 
     return{
+        page,
+        perPage,
+        total,
         totalPages,
         hasNextPage,
         hasPreviousPage,
