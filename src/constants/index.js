6d20773ba3db18cb368ec1a3836485e969552977
adelife <142346901+adelife@users.noreply.github.com>
2024-09-19 17:32:35 +0300

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ const contactFieldList = [
 
 export const ACCESS_TOKEN_TTL = 15*60*1000; // 15 min
 
-export const REFRESH_TOKEN_TTL = 24*60*60*1000; // це 1 день. треба 30 по завданню
+export const REFRESH_TOKEN_TTL = 24*30*60*60*1000; 
 
 
 export {contactType, sortOrderList, contactFieldList };
