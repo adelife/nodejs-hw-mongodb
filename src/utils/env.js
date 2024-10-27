7658357a56c3a8d9baabb75d9fe6382d0b3c3025
adelife <142346901+adelife@users.noreply.github.com>
2024-10-28 01:53:59 +0200

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -9,7 +9,8 @@ export function env(name, defaultValue) {
 
   if (defaultValue) return defaultValue;
 
-  throw new Error(`Missing: process.env[${name}].`);
+  // throw new Error(`Missing: process.env[${name}]`);
+  throw new Error(`Missing: process.env['${name}'].`);
 }
 
 // export default env;
