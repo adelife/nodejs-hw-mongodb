38a07bfe490d8ef4fee737b15acc468f7d9e0ce9
adelife <142346901+adelife@users.noreply.github.com>
2024-10-29 22:26:32 +0200

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,13 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'node:fs/promises';
 // import path from 'node:path';
-// import { CLOUDINARY } from '../constants/index.js';
+import { CLOUDINARY } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
 cloudinary.config({
-  cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-  api_key: env('CLOUDINARY_API_KEY'),
-  api_secret: env('CLOUDINARY_API_SECRET'),
+  cloud_name: env(CLOUDINARY.CLOUDINARY_CLOUD_NAME),
+  api_key: env(CLOUDINARY.CLOUDINARY_API_KEY),
+  api_secret: env(CLOUDINARY.CLOUDINARY_API_SECRET),
 });
 
 export const uploadToCloudinary = async (file) => {
